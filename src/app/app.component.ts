46861a4982ddd64cc0959404c7e9ef449c5ea387
RAKOTOARISOA Tahiriniaina Andrianina <ta.rakotoarisoa@soprasteria.com>
2024-08-29 17:43:51 +0200

    Init tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslateModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'localize-tutorial';
+
+  constructor(private translate: TranslateService) {
+    const userLanguage = navigator.language || 'en';
+    const languageCode = userLanguage.split('-')[0];
+    this.translate.setDefaultLang(languageCode);
+    this.translate.use(languageCode);
+  }
 }
